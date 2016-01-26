113a22a9ec83f917077a57d593a3d0994715b0b1
Jose Ignacio Rodríguez García <JoseIgnacio.Rodriguez@tajamar365.com>
2016-01-26 14:01:53 +0100

    as

---
+++ RedContactos/RedContactos/Util/Session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RedContactos.Util
+{
+    public class Session
+    {
+        private static Dictionary<String, Object> _session = new Dictionary<string, object>();
+
+        public Object this[String index]//indexor se usa para guardar sesiones en memoria (es decir guardar datos en memorea usuarios, configuraciones etc..)
+        {
+            get { return _session[index]; }
+
+            set { _session[index] = value; }
+        }
+    }
+}
