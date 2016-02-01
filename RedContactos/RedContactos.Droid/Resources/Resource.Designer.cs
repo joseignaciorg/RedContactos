cdb35187c18100784c8299fde65ca98ebd0f0258
Jose Ignacio Rodríguez García <JoseIgnacio.Rodriguez@tajamar365.com>
2016-02-01 11:17:10 +0100

    no message

--- RedContactos/RedContactos.Droid/Resources/Resource.Designer.cs
+++ RedContactos/RedContactos.Droid/Resources/Resource.Designer.cs
@@ -45,7 +45,13 @@ namespace RedContactos.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int icon = 2130837504;
+			public const int Contacto = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int icon = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Login = 2130837506;
 			
 			static Drawable()
 			{
